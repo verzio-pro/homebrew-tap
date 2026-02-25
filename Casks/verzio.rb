82fa0f712e2e391e1ac89650c48875256f78b052
jmpellizzer <joaomichel.pellizzer@gmail.com>
2026-02-25 14:06:06 +0100

    Update verzio to 1.0

--- Casks/verzio.rb
+++ Casks/verzio.rb
@@ -1,6 +1,6 @@
 cask "verzio" do
-  version "1.0.0"
-  sha256 "REPLACE_WITH_SHA256"
+  version "1.0"
+  sha256 "a59ad4c9901ad623896a7fde1ac9c22349c3c0a8964f178bc3b69b9d4a6d900f"
 
   url "https://github.com/jmpellizzer/Verzio/releases/download/v#{version}/Verzio.dmg"
   name "Verzio"
