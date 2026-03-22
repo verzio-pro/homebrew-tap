807de9c9b25b40dd236ec2650819cb182295648f
jmpellizzer <joaomichel.pellizzer@gmail.com>
2026-03-22 16:27:04 +0100

    Update verzio to 1.1.9

--- Casks/verzio.rb
+++ Casks/verzio.rb
@@ -1,9 +1,9 @@
 cask "verzio" do
-  arch arm: "arm64", intel: "x86_64"
+  arch arm: "arm64", intel: "eb65b39a2c2f166e4746df362a2cf058a26e2bb51734ea8eb11d33f11f17174e"
 
-  version "1.1.8"
-  sha256 arm:   "6e5791b5383a960e14a2774b5006fefb6a282b622aa0c832897eb40be37754c0",
-         intel: "4430c4f75dc904961d0b24306c9cedee905f03c951d9a442d4d944d5fae3ed3e"
+  version "1.1.9"
+  sha256 arm:   "65aaf271b5b16c6d6fa21cec9f49af7b29fd6938ab91860cde8079ced77fa619",
+         intel: "eb65b39a2c2f166e4746df362a2cf058a26e2bb51734ea8eb11d33f11f17174e"
 
   url "https://ztydsycmuuscuepyzvch.supabase.co/storage/v1/object/public/verzio-releases/Verzio-#{arch}.zip"
   name "Verzio"
